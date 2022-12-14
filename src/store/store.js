8ea95fb12d262799c44ac7791a2546dbe518b411
yeon <ohy9205@naver.com>
2022-12-15 03:46:13 +0900

    createReducer로 변경

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,15 @@
-import { configureStore, createAction } from "@reduxjs/toolkit";
+import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
 
 const addTodo = createAction("ADD");
 const deleteTodo = createAction("DELETE");
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addTodo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteTodo.type:
-      return state.filter((todo) => todo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+const reducer = createReducer([], {
+  [addTodo]: (state, action) => {
+    state.push({ text: action.payload, id: Date.now() });
+  },
+  [deleteTodo]: (state, action) =>
+    state.filter((todo) => todo.id !== action.payload),
+});
 
 const store = configureStore({ reducer: reducer });
 
